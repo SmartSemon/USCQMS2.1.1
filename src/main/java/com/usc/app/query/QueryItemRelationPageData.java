0edbbeb47c85b9827e75ebba3661ef38f678b588
SmartSemon <1207899173@qq.com>
2020-03-02 17:28:58 +0800

    修改关联页查询实现类

--- src/main/java/com/usc/app/query/QueryItemRelationPageData.java
+++ src/main/java/com/usc/app/query/QueryItemRelationPageData.java
@@ -1,23 +1,26 @@
 package com.usc.app.query;
 
-import java.util.List;
-
 import com.usc.app.action.a.AbstractRelationAction;
 import com.usc.app.action.mate.MateFactory;
-import com.usc.server.jdbc.DBUtil;
+import com.usc.app.action.utils.ActionMessage;
+import com.usc.app.entry.ret.RetSignEnum;
+import com.usc.app.query.search.i.AbstractSearchAction;
+import com.usc.obj.api.USCObject;
+import com.usc.obj.util.USCObjectQueryHelper;
 
-public class QueryItemRelationPageData extends AbstractRelationAction
-{
+public class QueryItemRelationPageData extends AbstractRelationAction implements AbstractSearchAction {
 
 	@Override
-	public Object executeAction() throws Exception
-	{
+	public Object executeAction() throws Exception {
 		String itemA = root.getItemNo();
 		String itemAID = root.getID();
 		String itemB = context.getItemNo();
 		String rel = MateFactory.getItemInfo(getRelationShip().getRelationItem()).getTableName();
-		List dataList = DBUtil.getRelationItemResult(itemA, MateFactory.getItemInfo(itemB), rel, itemAID, 1);
-		return this.queryTrue(dataList);
+		String condition = "DEL=0 and exists(select 1 from " + rel + " where DEL=0 and ITEMA='" + itemA + "' and "
+				+ "ITEMB='" + itemB + "' and ITEMBID=" + itemB + ".ID and ITEMAID='" + itemAID + ")";
+		USCObject[] objects = USCObjectQueryHelper.getObjectsByConditionLimit(itemB, condition, getDataPage(context));
+//		List dataList = DBUtil.getRelationItemResult(itemA, MateFactory.getItemInfo(itemB), rel, itemAID, 1);
+		return new ActionMessage(flagTrue, RetSignEnum.QUERY, "", objects);
 	}
 
 }
